2df48076ca5c89ffa8382f1560801ef8adb13508
Sachin Alam <sachinalam4216@gmail.com>
2024-07-04 11:42:10 +0530

    bg changer pilot

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
-  return (
-    <>
+  const [color, setColor] = useState("lightgrey");
 
-    </>
+  return (
+    <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
+
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+
+        <div className='flex flex-wrap justify-center gap-3 shadow-xl bg-white px-3 py-2 rounded-xl'>
+
+          <button onClick={() => setColor("red")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg border border-black' style={{ backgroundColor: "red" }}>Red</button>
+
+          <button onClick={() => setColor("green")}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg border border-black' style={{ backgroundColor: "green" }}>Green</button>
+
+          <button onClick={() => setColor("blue")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg border border-black' style={{ backgroundColor: "blue" }}>Blue</button>
+
+          <button onClick={() => setColor("yellow")} className='outline-none px-4 py-1 rounded-full text-black shadow-lg border border-gray-500' style={{ backgroundColor: "yellow" }}>Yellow</button>
+
+          <button onClick={() => setColor("orange")} className='outline-none px-4 py-1 rounded-full text-black shadow-lg border border-gray-500' style={{ backgroundColor: "orange" }}>Orange</button>
+
+          <button onClick={() => setColor("purple")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg border border-gray-500' style={{ backgroundColor: "purple" }}>Purple</button>
+
+          <button onClick={() => setColor("pink")} className='outline-none px-4 py-1 rounded-full text-black shadow-lg border border-gray-500' style={{ backgroundColor: "pink" }}>Pink</button>
+
+          <button onClick={() => setColor("brown")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg border border-gray-500' style={{ backgroundColor: "brown" }}>Brown</button>
+          
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
